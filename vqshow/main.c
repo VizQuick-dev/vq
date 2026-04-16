d11715be37a974eaac1dd090f5bb2ae882890626
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-16 20:31:01 +0200

    starting with vqshow

---
+++ vqshow/main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "../parse/filetype.h"
+
+int main(int argc, char *argv[])
+{
+    if(argc < 2){
+        printf("usage: vq <file>\n");
+	return 1;
+    }else{
+  	char *input_filename = argv[1];
+	int input_check = parse_check_file(input_filename);
+	
+	printf("%d",input_check)
+
+	return 0;
+    }
+}
