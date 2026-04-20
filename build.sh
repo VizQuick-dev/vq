bef45cd090d6395b5dc328fdc53b75aa04054071
JRBlockkop <161836282+JRBlockkop@users.noreply.github.com>
2026-04-20 18:21:07 +0200

    fixing foldername

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 gcc vqmain/main.c parse/*.c lib/pbm/*.c -o vq
 echo "== [ Compiled vq ] =="
-gcc vqshow/main.c parse/*.c lib/pbm/*.c -o vqshow
+gcc show/main.c parse/*.c lib/pbm/*.c -o vqshow
 echo "== [ Compiled vqshow ] == "
 read
